421fcc82da859cdac133a63433ca3b5e5577cc6c
Shardul Sankpal <niharsankpal4849@gmail.com>
2024-06-29 07:49:18 +0000

    learning Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "shardul@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState;
+
+accountEmail = "abc@abc.com"
+accountPassword = "212121212"
+accountCity = "Chennai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
